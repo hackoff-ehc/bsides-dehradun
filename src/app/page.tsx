411e3cf2296f115c83856c865530f7ad342a13d3
Aachman <achmanagarwal7696@gmail.com>
2023-10-31 20:20:51 +0530

    About us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ export const metadata = {
   title: "Rom Rom Rom",
 };
 
+import About from "@/components/AboutUs/About";
+
 export default function Home() {
-  return <h1>Rom rom</h1>;
+  return(
+    <div>
+      <About />
+    </div>
+  )
 }
