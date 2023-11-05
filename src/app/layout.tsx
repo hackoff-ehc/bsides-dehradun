2723543dc45c8bf7e110d2fe7bbf0e500d1fd010
iHarryD <anuraagvr555@gmail.com>
2023-11-05 14:25:46 +0530

    fix: remove interest section import in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components";
-import Interest from "../components/Home/Interest/Interest";
 
 const inter = Inter({ subsets: ["latin"] });
 
